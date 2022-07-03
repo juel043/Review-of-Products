b8d54c8b7e44a7de7ac5ec220db4020219073cf0
juel043 <juelhossain466@gmail.com>
2022-07-03 14:45:45 +0600

    database file

---
+++ review_table.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2022 at 10:45 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review_table`
+--
+
+CREATE TABLE `review_table` (
+  `review_id` int(11) NOT NULL,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int(1) NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `review_table`
+--
+
+INSERT INTO `review_table` (`review_id`, `user_name`, `user_rating`, `user_review`, `datetime`) VALUES
+(6, 'John Smith', 4, 'Nice Product, Value for money', 1621935691),
+(7, 'Peter Parker', 5, 'Nice Product with Good Feature.', 1621939888),
+(8, 'Donna Hubber', 1, 'Worst Product, lost my money.', 1621940010),
+(9, 'Md Juel Hossain', 3, 'nice', 1656827036),
+(10, 'Md Juel Hossain', 2, 'he is', 0),
+(11, 'Md Jubayer', 3, 'jkjk', 1656828184),
+(12, 'Abdullah Al Momin', 4, 'lkl', 2022),
+(13, 'Abdullah Al Momin', 4, 'jkjkjk', 2022),
+(14, 'Md Enamul Hossain', 4, 'mmm', 1656828603),
+(18, 'Md Juel Hossain', 3, 'Nice Products. Quality is very good ', 1656829199),
+(19, 'Md Mehedi ', 4, 'Its 100% Working', 1656836073);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `review_table`
+--
+ALTER TABLE `review_table`
+  ADD PRIMARY KEY (`review_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `review_table`
+--
+ALTER TABLE `review_table`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
